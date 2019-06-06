e671612472254272e4ae72b12f06d14456003209
Prabhjot Singh <pjsingh1359@outlook.com>
2019-06-06 13:07:53 -0700

    finished cat carasol

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
+'use strict';
 /*eslint-env jquery*/
 
-function handleThumbnailClicks() {
-  $('.thumbnail').on('click', function(event) {
-     const imgSrc = $(this).find('img').attr('src');  
-     const imgAlt = $(this).find('img').attr('alt');
-    
-    $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
-    
-    // Accomplishes the same as line 6 with only one use off .attr()
-    // by passing it an object with multiple properties.
-    // See: http://api.jquery.com/attr/#attr-attributes
-    // $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+function event(){
+  $('.thumbnail').on('click', e => {
+  
+    let selectedSrc = $(e.currentTarget).find('img').attr('src');
+  
+    $('.hero img').attr('src',selectedSrc)
   });
-}
-
-$(handleThumbnailClicks);
+};
+  
+$(event);
